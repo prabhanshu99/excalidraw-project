7d3d47a50435470c5739e3be8e5832648d65d3c5
Prabhanshu Narnaware <mrprabhanshu@gmail.com>
2026-01-06 22:02:30 +0530

    added some packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,17 +1,36 @@
 import express from "express"
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/be-common/config";
 import { middleware } from "./middleware";
+import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types"
 
 const app = express();
 
 app.post("/signup",(req,res)=>{
+    const data = CreateUserSchema.safeParse(req.body);
+
+    if(!data){
+        res.json({
+            message:"incorrect input"
+        })
+        return;
+    }
+
     res.json({
         userId : 123
     })
 })
 
 app.post("/signin",(req,res)=>{
+
+    const data = SigninSchema.safeParse(req.body);
+
+    if(!data){
+        res.json({
+            message:"incorrect input"
+        })
+        return;
+    }
 
     const userId = 1;
     const token = jwt.sign({
@@ -25,6 +44,16 @@ app.post("/signin",(req,res)=>{
 })
 
 app.post("/room",middleware,(req,res)=>{
+
+    const data = CreateRoomSchema.safeParse(req.body);
+
+    if(!data){
+        res.json({
+            message:"incorrect input"
+        })
+        return;
+    }
+
     res.json({
         roomId:123
     })
